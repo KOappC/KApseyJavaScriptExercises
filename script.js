d606c9a31ae0d53c04184a31c7a0baa3b64485be
Kyle Apsey <kyleapsey@Kyles-MacBook-Pro.local>
2017-10-04 20:51:46 -0400

    Updated with comments for the second time

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ for (var i=0; i<=a; i++)
 
 /*
 
-Prompt the user for a number. Write a for loop that will add all of the numbers leading up to that number, and inclusive of that number together and log the result to the console. (ex: A user enters 5, and the output would be the result of 1 + 2 + 3 + 4 + 5.)
+Prompt the user for a number. Write a for loop that will add all of the numbers leading up to that number,
+and inclusive of that number together and log the result to the console. (ex: A user enters 5, and the output
+would be the result of 1 + 2 + 3 + 4 + 5.)
 Prompt: ‘Enter A Number!’ > 5 Output: 15
 
 */
@@ -17,8 +19,14 @@ Prompt: ‘Enter A Number!’ > 5 Output: 15
 
 /*
 
-Write a do..while loop that builds a string with multiple inputs from a user. Start by prompting the user if they want to play. When the user answers yes, prompt the user to enter a word. After the user enters a word, prompt the user if they would like to play again. If no, console log their word. If yes, prompt the user for another word and add that word to the original string, then prompt if they want to play again. Continue to add words to the string every time the user answers yes and enters a word until the user indicates that he or she does not want to play anymore.
-Prompt: ‘Do you want to play?’ > 'yes' > Prompt: ‘Enter a word.’ > 'dog' > Prompt: ‘Do you want to play again?’ > 'yes' > Prompt: ‘Enter a word.' > 'cat' > Prompt: ‘Do you want to play again?’ > 'no' Output: 'dog cat'
+Write a do..while loop that builds a string with multiple inputs from a user. Start by prompting the user if
+they want to play. When the user answers yes, prompt the user to enter a word. After the user enters a word,
+prompt the user if they would like to play again. If no, console log their word. If yes, prompt the user for
+another word and add that word to the original string, then prompt if they want to play again. Continue to add
+words to the string every time the user answers yes and enters a word until the user indicates that he or she
+does not want to play anymore.
+Prompt: ‘Do you want to play?’ > 'yes' > Prompt: ‘Enter a word.’ > 'dog' > Prompt: ‘Do you want to play again?’
+> 'yes' > Prompt: ‘Enter a word.' > 'cat' > Prompt: ‘Do you want to play again?’ > 'no' Output: 'dog cat'
 
 */
 
@@ -26,8 +34,13 @@ Prompt: ‘Do you want to play?’ > 'yes' > Prompt: ‘Enter a word.’ > 'dog'
 
 /*
 
-Write a while loop that will prompt the user if they would like to print their name. If the answer is yes, log their name to the console then prompt them if they would like to print their name again. If yes, log their name to the console again but this time add an exclamation point at the end of the string. Continue to add an exclamation point for every time the user agrees to wanting to print his or her name.
-Prompt: ‘Would you like to print your name?’ > 'yes' Output: 'Hello. My name is Adam' Prompt: ‘Would you like to print this again?’ > 'yes' Output: ‘Hello. My name is Adam!' Prompt: ‘Would you like to print this again?’ > 'yes' Output: 'Hello. My name is Adam!!' Prompt: ‘Would you like to print this again?’ > 'no'
+Write a while loop that will prompt the user if they would like to print their name. If the answer is yes,
+log their name to the console then prompt them if they would like to print their name again. If yes, log their
+name to the console again but this time add an exclamation point at the end of the string. Continue to add an
+exclamation point for every time the user agrees to wanting to print his or her name.
+Prompt: ‘Would you like to print your name?’ > 'yes' Output: 'Hello. My name is Adam' Prompt: ‘Would you like to
+print this again?’ > 'yes' Output: ‘Hello. My name is Adam!' Prompt: ‘Would you like to print this again?’ >
+'yes' Output: 'Hello. My name is Adam!!' Prompt: ‘Would you like to print this again?’ > 'no'
 
 */
 
@@ -35,8 +48,12 @@ Prompt: ‘Would you like to print your name?’ > 'yes' Output: 'Hello. My name
 
 /*
 
-Prompt the user for a time of day (morning, noon, or evening). Based on their input, log a string to the console that will let the user know what they should be eating for that specific meal. Use the tables below to guide your logic.
+Prompt the user for a time of day (morning, noon, or evening). Based on their input, log a string to the console
+that will let the user know what they should be eating for that specific meal. Use the tables below to guide your
+logic.
 Example Outputs:
-Prompt: ‘What time of day is it?’ > ‘morning’ Output: ‘Since it is morning, you should be eating breakfast. We suggest eggs and toast.’ Prompt: ‘What time of day is it?’ > ‘evening’ Output: ‘Since it is evening, you should be eating dinner. We suggest chicken and rice.’
+Prompt: ‘What time of day is it?’ > ‘morning’ Output: ‘Since it is morning, you should be eating breakfast. We
+suggest eggs and toast.’ Prompt: ‘What time of day is it?’ > ‘evening’ Output: ‘Since it is evening, you should be
+eating dinner. We suggest chicken and rice.’
 
  */
